27232d81d25ba5d7622d4bdee4d279a7936898e4
maikeleerens <maikel@chaincode.eu>
2020-02-07 10:54:53 +0100

    Added C# problem 1 solution

---
+++ C#/Daily Coding Problems/Daily Coding Problem 1/DailyCodingProblem1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Daily_Coding_Problem_1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DailyCodingProblem1
+    {
+        [Fact]
+        public void DailyCodingProblem1SolutionTest()
+        {
+            var numberList = new List<int> {10, 15, 3, 7};
+            var k = 1000;
+
+            Assert.True(NumbersAreEqual(numberList, k));
+        }
+
+        private bool NumbersAreEqual(IEnumerable<int> numberList, int k)
+        {
+            return numberList.Select(number => k - number).Any(numberList.Contains);
+        }
+    }
+}
