81d20e83c2d284c570afb5d9dae57f5bd2a77ce1
maikeleerens <maikel@chaincode.eu>
2020-02-12 10:34:53 +0100

    updated C# problem 6 solution

--- C#/Solutions/Problem006/Problem006.cs
+++ C#/Solutions/Problem006/Problem006.cs
@@ -6,8 +6,12 @@ namespace Solutions.Problem006
 {
     /// <summary>
     /// This problem was asked by Google.
-    /// An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node.
+    /// 
+    /// An XOR linked list is a more memory efficient doubly linked list.
+    /// Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node.
+    /// 
     /// Implement an XOR linked list; it has an add(element) which adds the element to the end, and a get(index) which returns the node at index.
+    /// 
     /// If using a language that has no pointers(such as Python), you can assume you have access to get_pointer and dereference_pointer functions that converts between nodes and memory addresses.
     /// </summary>
     public class Problem006
@@ -59,7 +63,7 @@ namespace Solutions.Problem006
 
                 var nodeToAdd = new XORListNode<T>(_memoryAddress, currentNode.Address, value);
                 currentNode.Both ^= nodeToAdd.Address;
-                _memory.Add(_memoryAddress, currentNode);
+                _memory.Add(_memoryAddress, nodeToAdd);
             }
 
             _memoryAddress++;
